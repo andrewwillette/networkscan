c83b1634cb28cad186673a8e36c8fe25cc68526a
andrewwillette <willette.andrew1846@gmail.com>
2022-07-27 20:06:12 -0500

    master: learning about local addresses

---
+++ networkscan.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+
+}
+
+func getLocalIp() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
---
+++ networkscan_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetLocalIp(t *testing.T) {
+	lip := getLocalIp()
+	assert.Equal(t, "10.0.0.103", lip.String())
+}
